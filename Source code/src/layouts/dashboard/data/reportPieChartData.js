90b0bf7c32f353ec6ffafa15ee4dba3990d1bcf9
Hoanglion140901 <90514242+Hoanglion140901@users.noreply.github.com>
2023-09-30 14:16:14 +0700

    Delete Source code directory

--- Source code/src/layouts/dashboard/data/reportPieChartData.js
+++
@@ -1,8 +0,0 @@
-export default {
-  labels: ["Hàng Nhập", "Hàng Bán", "Hàng Còn"],
-  datasets: {
-    label: "DT",
-    data: [530, 240, 310],
-    backgroundColors: ["#bc5090", "#ff6361", "#ffa600"],
-  },
-};
